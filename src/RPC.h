940ab9627fe2b574b742fda295f54a55edb4a159
Yuhi Fukuhara <yuhi.fukuhara@cern.ch>
2018-07-17 20:25:12 +0900

    Change axis label

--- src/RPC.h
+++ src/RPC.h
@@ -1137,7 +1137,7 @@ void RPC::InitMdtHist(){
 void RPC::InitPtResidualHist(){
   h_PtResidual_pt  = new TH2F("h_PtResidual_pt",  "h_PtResidual_pt;Probe p_{T} [GeV];p_{T} residual;Counts",  100, 0,    100, 50,  -20, 20);
   h_PtResidual_eta = new TH2F("h_PtResidual_eta", "h_PtResidual_eta;Probe #eta;p_{T} residual;Counts",        100, -2.5, 2.5, 50,  -20, 20);
-  h_pt_vs_pt       = new TH2F("h_pt_vs_pt",       "h_pt_vs_pt;Probe p_{T} [GeV];L2MuonSA p_{T} [GeV];Counts", 100, 0,    100, 100, 0,   100);
+  h_pt_vs_pt       = new TH2F("h_pt_vs_pt",       "h_pt_vs_pt;Probe Offline p_{T} [GeV];Probe L2MuonSA p_{T} [GeV];Counts", 100, 0,    100, 100, 0,   100);
 }
 
 void RPC::InitHist(){
