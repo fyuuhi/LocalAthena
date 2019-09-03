fea00d3e0591474f160bec2030eec70c49ceb6f3
Yuhi Fukuhara <yuhi.fukuhara@cern.ch>
2019-09-03 21:33:28 +0900

    Delete enableRobRequestPreparation=False

--- run/bsub_run.sh
+++ run/bsub_run.sh
@@ -11,13 +11,15 @@ DATE=$(date '+%Y%m%d%H%M')
 #INPUT_LIST="../Jpsi_424103.list"
 #INPUT_LIST="../Jpsi_424108.list"
 #INPUT_LIST="../Jpsi_424100.list"
-INPUT_LIST="../FTK_361107.list"
+#INPUT_LIST="../FTK_361107.list"
+#INPUT_LIST="../Jpsi_PGE.list"
+INPUT_LIST="../Jpsi_PGE_d1534.list"
 #INPUT_RDO_FOR_OUTPUT="user.yfukuhar.mc16_13TeV.Zmumu"
 #OUTPUT_AOD=$INPUT_RDO_FOR_OUTPUT".AOD."$DATE".root"
 TEMP_RUN_SCRIPT="bsub_run_temp.sh"
 #MAXEVENTS='100'
-LABEL=DIR_RDO_FTK_Zmumu
-#LABEL=DIR_RDO_FTK_Jmumu
+#LABEL=DIR_RDO_FTK_Zmumu
+LABEL=DIR_RDO_FTK_Jmumu
 
 echo ""
 echo "INPUT_LIST =   "$INPUT_LIST
@@ -49,7 +51,7 @@ if [ -f "$INPUT_LIST" ] ; then
     cd $DIR
     sed -i -e "10i  \ \ --inputRDO_FTKFile $INPUT_FILE \ \\\\" $TEMP_RUN_SCRIPT
     echo "=========================="
-    bsub -n 3 -q 1d -o log_out ./bsub_run_temp.sh
+    bsub -n 2 -q 1d -o log_out ./bsub_run_temp.sh
     echo "=========================="
     echo ""
 
