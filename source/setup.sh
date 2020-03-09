69a8eba263382b244836086d8a6ee503afe2cbe0
Yuhi Fukuhara <yuhi.fukuhara@cern.ch>
2020-03-09 16:59:05 +0900

    Update

--- source/setup.sh
+++ source/setup.sh
@@ -31,7 +31,8 @@ setupATLAS
 #asetup Athena,master,2019-12-09T2135,here
 #asetup Athena,master,2019-12-09T2135,here
 #asetup Athena,master,2019-12-22T2131,here
-asetup Athena,master,2020-01-25T2132,here
+#asetup Athena,master,2020-01-25T2132,here
+asetup Athena,master,2020-03-04T2134,here
 #asetup Athena,master,latest,here
 #asetup Athena,22.0.8,here
 #asetup Athena,21.3.15,here
